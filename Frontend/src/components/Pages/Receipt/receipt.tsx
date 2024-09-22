38b5186f0071fd7fa76d4de82ed05cef6f564005
ThanawitGZS <Tanawit5578@gmail.com>
2024-09-22 17:22:07 +0700

    Update receipt.tsx

--- Frontend/src/components/Pages/Receipt/receipt.tsx
+++ Frontend/src/components/Pages/Receipt/receipt.tsx
@@ -1,13 +1,19 @@
 import { Link  } from 'react-router-dom';
-import { Card, Table, Col, Row, Statistic, Button , message } from 'antd';
+import { useNavigate } from "react-router-dom"; 
+import { Card, Table, Col, Row, Statistic, Button , message , Spin , Empty , Badge, } from 'antd';
 import { WalletOutlined, FileSyncOutlined, FileDoneOutlined, UserOutlined } from "@ant-design/icons";
 import { ReceiptInterface } from "../../../interfaces/Receipt";
-import { GetReceipts , GetTables } from "../../../services/https";
+import { TableInterface } from '../../../interfaces/Table';
+import { TableStatusInterface } from '../../../interfaces/TableStatus';
+import { GetReceipts , GetTables , GetTableStatus } from "../../../services/https";
 import { useState, useEffect } from "react";
 import type { ColumnsType } from "antd/es/table";
 import dayjs from "dayjs";
 
 function Receipt() {
+  const [tables, setTables] = useState<TableInterface[]>([]);
+  const [tableStatus, setTableStatus] = useState<TableStatusInterface[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
 
   const [receipt, setReceipt] = useState<ReceiptInterface[]>([]);
   const [HoldValue, setHoldValue] = useState<number>(0);
@@ -72,85 +78,160 @@ function Receipt() {
   };
 
   useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const [tablesRes , statusRes] = await Promise.all([
+          GetTables(),
+          GetTableStatus(),
+        ]);
+
+        if (tablesRes.status === 200) {
+          setTables(tablesRes.data);
+        } else {
+          message.error(tablesRes.data.error || "Unable to fetch tables");
+        }
+
+        if (statusRes.status === 200) {
+          setTableStatus(statusRes.data);
+        } else {
+          message.error(statusRes.data.error || "Unable to fetch table statuses");
+        }
+
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        message.error("Error fetching data from the server");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const handleButtonClick = (table: TableInterface) => {
+    if (!table.ID || !table.table_capacity_id) {
+      message.warning("Table ID or Table capacity ID is not defined!");
+      return;
+    }
+
+    const status = tableStatus.find((s) => s.ID === table.table_status_id)?.status;
+
+    if (status === "Avaliable") {
+      message.warning("This table is available for booking!");
+      return;
+    }
+    if (status === "Cleaning") {
+      message.warning("This table is not already for booking!");
+      return;
+    }
+
+    if (table.table_name) {
+      const params = new URLSearchParams({
+        tableId: table.ID.toString(),
+        tableName: table.table_name,
+        tableStatus: tableStatus.toString(),
+      }).toString();
+
+      window.location.href = `/receipt/pay?${params}`;
+    } else {
+      message.warning("This table does not have a defined type!");
+    }
+  };
+
+  const getStatusColor = (status: string): string => {
+    switch (status) {
+      case "Available":
+        return "#8BC34A"; 
+      case "Occupied":
+        return "#FFB74D"; 
+      case "Cleaning":
+        return "#64B5F6"; 
+      default:
+        return "#B0BEC5";
+    }
+  };
+
+  useEffect(() => {
     getReceipts();
     FetchHoldData();
     FetchSuccessData();
     FetchTotalPrice();
   }, []);
 
-  const paths = [
-    "/receipt/pay",
-    "/receipt/pay",
-    "/receipt/pay",
-    "/receipt/pay",
-    "/receipt/pay",
-    "/receipt/pay",
-    "/receipt/pay",
-    "/receipt/pay",
-    "/receipt/pay",
-    "/receipt/pay",
-    "/receipt/pay",
-    "/receipt/pay"
-  ];
+  // const paths = [
+  //   "/receipt/pay",
+  //   "/receipt/pay",
+  //   "/receipt/pay",
+  //   "/receipt/pay",
+  //   "/receipt/pay",
+  //   "/receipt/pay",
+  //   "/receipt/pay",
+  //   "/receipt/pay",
+  //   "/receipt/pay",
+  //   "/receipt/pay",
+  //   "/receipt/pay",
+  //   "/receipt/pay"
+  // ];
 
-  const buttonLabels = [
-    "Table : F1",
-    "Table : F2",
-    "Table : F3",
-    "Table : F4",
-    "Table : F5",
-    "Table : S1",
-    "Table : S2",
-    "Table : S3",
-    "Table : S4",
-    "Table : E1",
-    "Table : E2",
-    "Table : E3"
-  ];
+  // const buttonLabels = [
+  //   "Table : F1",
+  //   "Table : F2",
+  //   "Table : F3",
+  //   "Table : F4",
+  //   "Table : F5",
+  //   "Table : S1",
+  //   "Table : S2",
+  //   "Table : S3",
+  //   "Table : S4",
+  //   "Table : E1",
+  //   "Table : E2",
+  //   "Table : E3"
+  // ];
 
-  const tableseat = [
-    ": 1 - 4",
-    ": 1 - 4",
-    ": 1 - 4",
-    ": 1 - 4",
-    ": 1 - 4",
-    ": 5 - 6",
-    ": 5 - 6",
-    ": 5 - 6",
-    ": 5 - 6",
-    ": 7 - 8",
-    ": 7 - 8",
-    ": 7 - 8"
-  ];
+  // const tableseat = [
+  //   ": 1 - 4",
+  //   ": 1 - 4",
+  //   ": 1 - 4",
+  //   ": 1 - 4",
+  //   ": 1 - 4",
+  //   ": 5 - 6",
+  //   ": 5 - 6",
+  //   ": 5 - 6",
+  //   ": 5 - 6",
+  //   ": 7 - 8",
+  //   ": 7 - 8",
+  //   ": 7 - 8"
+  // ];
 
-  const buttons = paths.map((path, index) => (
-    <Col key={index} xs={24} sm={12} md={8} lg={6} style={{ marginBottom: '16px' }}>
-      <Link to={path}>
-        <Button
-          // className="custom-button"
-          style={{
-            width: '100%',
-            height: '90px',
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            justifyContent: 'center',
-            borderRadius: '10px',
-            boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.2)',
-            padding: '10px',
-            border: '1px solid #d9d9d9'
-          }}
-        >
-          <Statistic
-            title={buttonLabels[index]}
-            value={tableseat[index]}
-            prefix={<UserOutlined />}
-            valueStyle={{ fontSize: '16px' }}
-          />
-        </Button>
-      </Link>
-    </Col>
-  ));
+  // const buttons = paths.map((path, index) => (
+  //   <Col key={index} xs={24} sm={12} md={8} lg={6} style={{ marginBottom: '16px' }}>
+  //     <Link to={path}>
+  //       <Button
+  //         // className="custom-button"
+  //         style={{
+  //           width: '100%',
+  //           height: '90px',
+  //           display: 'flex',
+  //           flexDirection: 'column',
+  //           alignItems: 'center',
+  //           justifyContent: 'center',
+  //           borderRadius: '10px',
+  //           boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.2)',
+  //           padding: '10px',
+  //           border: '1px solid #d9d9d9'
+  //         }}
+  //       >
+  //         <Statistic
+  //           title={buttonLabels[index]}
+  //           value={tableseat[index]}
+  //           prefix={<UserOutlined />}
+  //           valueStyle={{ fontSize: '16px' }}
+  //         />
+  //       </Button>
+  //     </Link>
+  //   </Col>
+  // ));
 
   const columns: ColumnsType<ReceiptInterface> =[
     {
@@ -272,11 +353,66 @@ function Receipt() {
           </Row>
         </Card>
         <Card style={{ borderRadius: '20px', height: 'auto' }}>
-          <Card style={{ backgroundColor: "#F5F5F5", height: '100%' , borderRadius:'20px' }}>
+          {/* <Card style={{ backgroundColor: "#F5F5F5", height: '100%' , borderRadius:'20px' }}>
             <Row gutter={[16, 16]}>
               {buttons}
             </Row>
-          </Card>
+          </Card> */}
+          <Col xs={24}>
+            <Card className="card">
+              {loading ? (
+                <Spin tip="Loading..." className="spinContainer" />
+              ) : tables.length === 0 ? (
+                <Empty description="No tables available" className="emptyState" />
+              ) : (
+                <Row gutter={[16, 8]} justify="center" align="middle">
+                  {tables.map((table) => {
+                    const status = tableStatus.find(
+                      (status) => status.ID === table.table_status_id
+                    );
+
+                    return (
+                      <Col key={table.ID} xs={24} sm={12} md={8} lg={6}>
+                        <Button
+                          type="default"
+                          className="tableButton"
+                          onClick={() => handleButtonClick(table)}
+                          disabled={
+                            !table.table_name ||
+                            status?.status === "Available" ||
+                            status?.status === "Cleaning"
+                          }
+                          style={{
+                            width: "100%",
+                            display: "flex",
+                            justifyContent: "space-between",
+                            alignItems: "center",
+                            textAlign: "left",
+                          }}
+                        >
+                          <Statistic className="statistic-container"
+                            // title={`Table : ${table.table_name || "Unknown"}`}
+                            value={"Table : "+table.table_name}
+                            valueStyle={{ color: "#FF7F50" }}
+                            prefix={<UserOutlined className="icon" />}
+                          />
+                          <Badge
+                            count={status?.status ?? "Unknown"}
+                            style={{
+                              backgroundColor: getStatusColor(
+                                status?.status ?? "Unknown"
+                              ),
+                              color: "#fff",
+                            }}
+                          />
+                        </Button>
+                      </Col>
+                    );
+                  })}
+                </Row>
+              )}
+            </Card>
+          </Col>
         </Card>
       </Col>
     </Row>

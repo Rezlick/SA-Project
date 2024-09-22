c61e3da9ea62ded9532e87816daa0fc40d90242c
ThanawitGZS <Tanawit5578@gmail.com>
2024-09-22 13:36:55 +0700

    Update fullLayout.tsx

--- Frontend/src/components/FullLayout/fullLayout.tsx
+++ Frontend/src/components/FullLayout/fullLayout.tsx
@@ -13,7 +13,8 @@ import ITSider from "../Sider/ITSider";
 import ManagerSider from "../Sider/ManagerSider";
 import CommonSider from "../Sider/sider";
 import ProfileEdit from "../Pages/ProfileEdit/profileEdit";
-import Payment from "../Pages/Payment/payment";
+import Receipt from "../Pages/Receipt/receipt";
+import Pay from "../Pages/Receipt/Pay/pay";
 import ChangePassword from "../Pages/ProfileEdit/changePassword";
 import Table from "../Pages/Booking/booking";
 import TableBooking from "../Pages/Booking/Create/createBooking";
@@ -77,7 +78,8 @@ const FullLayout: React.FC = () => {
               <Route path="/employee/edit/:id" element={<EmployeeEdit />} />
               <Route path="/profileEdit" element={<ProfileEdit />} />
               <Route path="/changePassword" element={<ChangePassword />} />
-              <Route path="/payment" element={<Payment />} />
+              <Route path="/receipt" element={<Receipt />} />
+//            <Route path="/receipt/pay" element={<Pay />} />
               <Route path="/booking" element={<Table />} />
               <Route path="/booking/create" element={<TableBooking />} />
               <Route path="/booking/edit/:id" element={<EditBookingTable />} />

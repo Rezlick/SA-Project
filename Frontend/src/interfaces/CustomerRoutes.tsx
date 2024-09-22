8820209083867c371e0d7c3a7121a5ee632b43fb
A12ise <sorrawit456flookza@gmail.com>
2024-09-22 14:57:44 +0700

    Create CustomerRoutes.tsx

---
+++ Frontend/src/interfaces/CustomerRoutes.tsx
@@ -0,0 +1,18 @@
+import { lazy } from "react";
+import React from "react";
+import { RouteObject } from "react-router-dom";
+import Loadable from "../components/third-party/Loadable";
+// import CustomerLayout from "../components/CustomerLayout/index";
+
+
+const CustomerPages = Loadable(lazy(() => import("../components/Pages/customer")));
+
+const CustomerRoutes = (): RouteObject => {
+    return {
+      path: "/customer",
+      element: <CustomerPages />,
+      
+    };
+  };
+  
+  export default CustomerRoutes;

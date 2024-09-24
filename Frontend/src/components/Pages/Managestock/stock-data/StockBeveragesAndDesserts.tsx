d54f4728ecdd250d34e4ab8c8945b9ef58014a67
jarntae <poramet2022@gmail.com>
2024-09-25 01:28:58 +0700

    Update StockBeveragesAndDesserts.tsx

--- Frontend/src/components/Pages/Managestock/stock-data/StockBeveragesAndDesserts.tsx
+++ Frontend/src/components/Pages/Managestock/stock-data/StockBeveragesAndDesserts.tsx
@@ -1,19 +1,16 @@
 import StockCategory from "../Category/StockCategory/StockCategory";
 import useStockData from "../../../..//Hook/useStockData";
-import useGetProductByCodeID from "../../../..//Hook/useGetProductByCodeID";
-//import useSupplierData from '../../Hook/useSupplierData';
+
 
 export default function StockBeveragesAndDesserts() {
   const id = 6;
-  const beveragesAndDessertsData = useStockData(id); // ดึงข้อมูล Stock ด้วย categoryID = 6
-  //const SupplierData = useSupplierData(); // ดึงข้อมูล Supplier
-  console.log("beveragesAndDessertsData : ",beveragesAndDessertsData);
-  console.log("useGetProductByCodeID",useGetProductByCodeID("B001"));
+  const beveragesAndDessertsData = useStockData(id); 
+
   
   
   return (
     <StockCategory
-      categoryTitle="เครื่องดื่มและขนมหวาน (Beverages and Desserts)"
+      categoryTitle="เครื่องดื่มและขนมหวาน "
       initialData={beveragesAndDessertsData}
       categoryID = {id}
       path = {"BeveragesAndDesserts"}
